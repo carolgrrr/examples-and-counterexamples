4e4e134a310e5fb7715020af1233ad6ea31acdcd
Carol Schmitz <carolgrrr@gmail.com>
2017-11-14 18:23:11 -0600

    add mongo connection

--- examples/findex/views.py
+++ examples/findex/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+import mongoengine
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the find example index.")
+
+	mydb = mongoengine.connect(db='examples')
+	#examples = mydb.objects()
+	#print(examples)
+	#count = examples.count()
+
+	#print("There are %d examples in the database." % count)
+	#qry = mongoengine.queryset.QuerySet()
+	return HttpResponse("Hello, world. You're at the find example index.")
